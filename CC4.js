e2fcc12e10cdc6c2750d936dc8cddbe5a793b6ab
Vyshalini02 <venkata.vyshalinisomisetty@cognizant.com>
2024-07-11 19:41:31 +0530

    updated code1

--- CC4.js
+++ CC4.js
@@ -10,3 +10,8 @@ if (bill > 50 && bill < 300) {
     total = bill + tip;
     console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}.`);
 }
+
+// Using ternary operator
+const bill2 = 430;
+const tip1 = bill2 > 50 && bill2 < 300 ? (15 / 100) * bill2 : (20 / 100) * bill2;
+console.log(`The bill was ${bill2}, the tip was ${tip1}, and the total value ${bill2 + tip1}.`);
